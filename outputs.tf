8c090792e0649c3749c97fefc4de13cb021af3de
Ignacio Ballester <62849783+ignacioballester@users.noreply.github.com>
2024-09-22 10:45:48 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "created_file_path" {
   description = "The path of the created file"
   value       = local_file.example_file.filename
 }
-
-output "created_directory_path" {
-  description = "The path of the created directory"
-  value       = local_file.example_directory.filename
-}
