553338aaf18bd711265722b2d51d37cdd91004b5
Ignacio Ballester <62849783+ignacioballester@users.noreply.github.com>
2024-10-10 18:32:46 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,21 @@ variable "directory_path" {
   description = "The path where the directory will be created"
   type        = string
 }
+
+variable "complex_variable" {
+  type = object({
+    name         = string
+    enabled      = bool
+    retries      = number
+    settings     = map(object({
+      speed        = number
+      acceleration = bool
+    }))
+    map          = map(string) 
+    tags         = list(string)
+    config       = object({
+      size  = number
+      scale = bool
+    })
+  })
+}
