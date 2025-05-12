0bb8644ec4a5c1b0796360464d089e2ac239300a
Ignacio Ballester <62849783+ignacioballester@users.noreply.github.com>
2025-05-12 18:32:59 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,55 +14,5 @@ variable "file_content" {
 
 variable "directory_path" {
   type        = string
-  description = <<EOD
-The path where the directory will be created
-@options_from_function
-function getList() {
-  return ['value1', 'value2'];
+  description = "The path where the directory will be created"
 }
-EOD
-}
-
-variable "complex_variable" {
-  type = object({
-    name         = string
-    enabled      = bool
-    retries      = number
-    settings     = map(object({
-      speed        = number
-      acceleration = bool
-    }))
-    map          = map(string) 
-    tags         = list(string)
-    config       = object({
-      size  = number
-      scale = bool
-    })
-  })
-  
-  default = {
-    name     = "default_name"
-    enabled  = true
-    retries  = 3
-    settings = {
-      setting1 = {
-        speed        = 100
-        acceleration = true
-      }
-      setting2 = {
-        speed        = 50
-        acceleration = false
-      }
-    }
-    map = {
-      key1 = "value1"
-      key2 = "value2"
-    }
-    tags = ["tag1", "tag2", "tag3"]
-    config = {
-      size  = 10
-      scale = true
-    }
-  }
-}
-
